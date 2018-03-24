6cdb02131579ba4fa22ca1d19312856bf5d19c26
andoshin11 <shinglish11@gmail.com>
2018-03-24 22:01:50 +0900

    Add babel config on nuxt

--- generator/template/nuxt.config.js
+++ generator/template/nuxt.config.js
@@ -7,7 +7,7 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '{{escape description }}' }
+      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
@@ -20,5 +20,9 @@ module.exports = {
   /*
   ** Build configuration
   */
-  build: {}
+  build: {
+    babel: {
+      presets: ["@vue/app"]
+    }
+  }
 }
