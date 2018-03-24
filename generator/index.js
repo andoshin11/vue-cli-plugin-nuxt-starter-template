18a70ed9b312f04c89c2ee5d1e2e2cb9d2133334
andoshin11 <shinglish11@gmail.com>
2018-03-24 22:12:22 +0900

    Testing postProcessFiles api

--- generator/index.js
+++ generator/index.js
@@ -21,4 +21,20 @@ module.exports = api => {
   }
 
   api.render('./template')
+
+  api.postProcessFiles(files => {
+    console.log('shin test')
+    const isTS = 'src/main.ts' in files
+    const file = isTS
+      ? 'src/main.ts'
+      : 'src/main.js'
+    const main = files[file]
+    if (main) {
+      // inject import for registerServiceWorker script into main.js
+      const lines = main.split(/\r?\n/g)
+      // const lastImportIndex = lines.findIndex(line => line.match(/^import/))
+      lines[-1] += `\nI'm testing`
+      files[file] = lines.join('\n')
+    }
+  })
 }
